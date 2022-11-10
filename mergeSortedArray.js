7793c5ece5076e758fbec83d67cd44d0bfc11a25
lordSosa15 <97013669+lordSosa15@users.noreply.github.com>
2022-11-09 23:09:30 -0500

    merge sorted array while or for loop

--- mergeSortedArray.js
+++ mergeSortedArray.js
@@ -19,22 +19,43 @@
 // The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
 
 function merge(nums1, m, nums2, n) {
-    let first = m - 1
-    let second = n - 1
-    let i = m + n -1
+  let first = m - 1;
+  let second = n - 1;
+  let i = m + n - 1;
 
-    while (second >= 0) {
-        let fVal = nums1[first]
-        let sVal = nums2[second]
+  while (second >= 0) {
+    let fVal = nums1[first];
+    let sVal = nums2[second];
 
-        if (fVal > sVal) {
-            nums1[i] = fVal;
-            i--;
-            first--;
-        } else {
-            nums1[i] = sVal;
-            i--;
-            second--
-        }
+    if (fVal > sVal) {
+      nums1[i] = fVal;
+      i--;
+      first--;
+    } else {
+      nums1[i] = sVal;
+      i--;
+      second--;
     }
+  }
+}
+
+time: O(n);
+space: O(1);
+
+// easiest solution
+function merge(nums1, m, nums2, n) {
+  let first = m - 1;
+  let second = n - 1;
+  for (let i = m + n - 1; i >= 0; i--) {
+    if (second < 0) {
+      break;
+    }
+    if (nums1[first] > nums2[second]) {
+      nums1[i] = nums1[first];
+      first--;
+    } else {
+      nums1[i] = nums2[second];
+      second--;
+    }
+  }
 }
